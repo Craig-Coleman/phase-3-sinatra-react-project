83733fadda7638b92b338f79cb36d934ddbb220a
Craig <craig.coleman.cc@gmail.com>
2022-06-13 13:18:58 -0500

    Create seed data and seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,22 @@
-puts "🌱 Seeding spices..."
+puts "🌱 Seeding data..."
 
-# Seed your database here
+List.create(name: "List 1")
+List.create(name: "List 2")
+List.create(name: "My List")
+
+Item.create(name: "Pepper", category: "condiment", price: 1, list_id: 1)
+Item.create(name: "lettuce", category: "produce", price: 2, list_id: 1)
+Item.create(name: "frozen waffles", category: "frozen", price: 3, list_id: 1)
+Item.create(name: "ketchup", category: "condiment", price: 1.50, list_id: 1)
+
+Item.create(name: "Pepper", category: "condiment", price: 1, list_id: 2)
+Item.create(name: "lettuce", category: "produce", price: 2, list_id: 2)
+Item.create(name: "frozen waffles", category: "frozen", price: 3, list_id: 2)
+Item.create(name: "ketchup", category: "condiment", price: 1.50, list_id: 2)
+
+Item.create(name: "Pepper", category: "condiment", price: 1, list_id: 3)
+Item.create(name: "lettuce", category: "produce", price: 2, list_id: 3)
+Item.create(name: "frozen waffles", category: "frozen", price: 3, list_id: 3)
+Item.create(name: "ketchup", category: "condiment", price: 1.50, list_id: 3)
 
 puts "✅ Done seeding!"
